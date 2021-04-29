48dad254846678b6ade1f07dcee23416e5852759
Tsedeke Nigusu <tsedekenigusu339@example.com>
2021-04-29 23:02:05 +0300

    additional python-import_modules py ans added

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import dis
+# if __name__ == "__main__":
+#     pass
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+    for element in range(98):
+        add(4, 6)
+    
+dis.dis(magic_calculation)
